d43feb679361b45144374bc8bcd10d8d3af52611
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-07-06 14:34:41 +0200

    Add pigs!

--- src/CGI/CGI.cpp
+++ src/CGI/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/01 11:51:45 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/06/30 15:08:23 by pvan-dij      ########   odam.nl         */
+/*   Updated: 2022/06/30 15:27:03 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static std::vector<std::string> get_envp(const ft::Request& request, const std::
 	std::vector<std::string> envp = {
 		"GATEWAY_INTERFACE=CGI/1.1", 
 		"SERVER_PROTOCOL=HTTP/1.1",
-		ft::format("REMOTE_ADDR=%d", -1),
+		ft::format("REMOTE_ADDR=%d", -1), // TODO: Where ????
 		ft::format("REQUEST_METHOD=%s", methods.find(request.method)), // 
 		ft::format("SCRIPT_NAME=%s", path.c_str()),
 		ft::format("SERVER_NAME=%s", request.getValue("server_names")->c_str()), // TODO: only get one server name (the first one)
