4ae60cefff78429d6ec91f9fa441794b2273295b
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-08-18 19:33:18 +0200

    add more forbidden fields for globalSection

--- src/Config/GlobalConfig.cpp
+++ src/Config/GlobalConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/01 14:59:11 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/08/18 19:17:00 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/08/18 19:31:19 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,19 +121,16 @@ static void checkNoServerKeysInSection(const uint32_t& lineNum, const ft::Sectio
 
 static void verifyGlobalSection(const uint32_t& lineNum, const ft::Section& globalSection)
 {
-	// check for fields that are required in the global section
-	// DISABLED, NOTHING IS REQUIRED!
-	/*
-	std::array<std::string, 1> requiredFields = { "limit_body_size" };
-	for (std::string& requiredField : requiredFields)
-	{
-		if (!globalSection.keyExists(requiredField))
-			throw ft::MissingFieldException(lineNum, requiredField, "global");
-	}
-	*/
-
 	// check for fields that are only allowed in server sections
 	checkNoServerKeysInSection(lineNum, globalSection, "global");
+
+	// check for more forbidden fields
+	std::array<std::string, 3> forbiddenFields = { "path", "redir" };
+	for (std::string& forbiddenField : forbiddenFields)
+	{
+		if (globalSection.keyExists(forbiddenField))
+			throw ft::ForbiddenFieldException(lineNum, forbiddenField, "global");
+	}
 }
 
 static void verifyLocationSection(const uint32_t& lineNum, const ft::Section& location)
